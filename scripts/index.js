6c59bb1bba75dfda34093025af416aab283faf58
firnandadc <102499139+firnandadc@users.noreply.github.com>
2026-06-09 13:32:40 +0700

    Change login username and success message
    Updated login credentials and success message.

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,11 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "user" && password === "user") {
+  if (username === "sasikirana" && password === "18102022") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang sayanggg cantik",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
